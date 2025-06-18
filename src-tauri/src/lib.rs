0ee5ab7b132fcd8f5472be25b52a881ae7f09b1b
Maxime BOURAND <shirowderable@gmail.com>
2025-06-18 08:09:45 +0200

    Add some logs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
+use std::io::{Cursor, Write};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
-use std::io::{Cursor, Write};
 
 #[tauri::command]
 async fn import_strings(
@@ -10,27 +10,50 @@ async fn import_strings(
     utmt_cli_folder_path: &str,
     git_chapter_folder_path: &str,
     git_root_folder_path: &str,
-	output_data_win_path: &str,
+    output_data_win_path: &str,
 ) -> Result<(), String> {
-	let import_script_path = format!("{}/Script/UMT/DRFR/ImporterToutTranslationTool.csx", git_root_folder_path);
-	let utmt_cli_exe_path = format!("{}/UndertaleModCli.exe", utmt_cli_folder_path);
+    let import_script_path = format!(
+        "{}/Script/UMT/DRFR/ImporterToutTranslationTool.csx",
+        git_root_folder_path
+    );
+    let utmt_cli_exe_path = format!("{}/UndertaleModCli.exe", utmt_cli_folder_path);
 
-	let mut utmt_command = Command::new(utmt_cli_exe_path)
-	.args(["load", &source_data_win_path, "-s", &import_script_path, "-o", &output_data_win_path])
-	.stdin(Stdio::piped())
-	.spawn()
-	.map_err(|e| format!("Failed to start UndertaleModCli command: {}", e))?;
+    let mut utmt_command = Command::new(utmt_cli_exe_path)
+        .args([
+            "load",
+            &source_data_win_path,
+            "-s",
+            &import_script_path,
+            "-o",
+            &output_data_win_path,
+        ])
+        .stdin(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("Failed to start UndertaleModCli command: {}", e))?;
 
-	let mut stdin = utmt_command.stdin.take().ok_or("Failed to open stdin for UndertaleModCli command")?;
+    let mut stdin = utmt_command
+        .stdin
+        .take()
+        .ok_or("Failed to open stdin for UndertaleModCli command")?;
 
-	let git_chapter_folder_path_owned = git_chapter_folder_path.to_owned();
+    let git_chapter_folder_path_owned = git_chapter_folder_path.to_owned();
     std::thread::spawn(move || {
-        let _ = stdin.write_all((git_chapter_folder_path_owned + "\n").as_bytes())
-            .map_err(|e| format!("Failed to write base data directory to UndertaleModCli stdin: {}", e));
-        let _ = stdin.flush().map_err(|e| format!("Failed to flush UndertaleModCli stdin: {}", e));
+        let _ = stdin
+            .write_all((git_chapter_folder_path_owned + "\n").as_bytes())
+            .map_err(|e| {
+                format!(
+                    "Failed to write base data directory to UndertaleModCli stdin: {}",
+                    e
+                )
+            });
+        let _ = stdin
+            .flush()
+            .map_err(|e| format!("Failed to flush UndertaleModCli stdin: {}", e));
     });
 
-	utmt_command.wait().map_err(|e| format!("Failed to wait for UndertaleModCli command: {}", e))?;
+    utmt_command
+        .wait()
+        .map_err(|e| format!("Failed to wait for UndertaleModCli command: {}", e))?;
 
     Ok(())
 }
@@ -38,7 +61,8 @@ async fn import_strings(
 #[tauri::command]
 async fn run_game_executable(game_folder_path: &str) -> Result<(), String> {
     let executable_path = format!("{}/DELTARUNE.exe", game_folder_path);
-    std::process::Command::new(executable_path).current_dir(game_folder_path)
+    std::process::Command::new(executable_path)
+        .current_dir(game_folder_path)
         .spawn()
         .map_err(|e| format!("Failed to start the game executable: {}", e))
         .map(|_| ())
@@ -59,13 +83,18 @@ fn unzip_file(path: String, target_dir: String) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![run_game_executable, import_strings, unzip_file])
+        .invoke_handler(tauri::generate_handler![
+            run_game_executable,
+            import_strings,
+            unzip_file
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,31 @@ import App from './App'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log'
+
+function forwardConsole(
+  fnName: 'log' | 'debug' | 'info' | 'warn' | 'error',
+  logger: (message: string) => Promise<void>
+) {
+  const original = console[fnName]
+  console[fnName] = (message) => {
+    original(message)
+    logger(message)
+  }
+}
+
+forwardConsole('log', trace)
+forwardConsole('debug', debug)
+forwardConsole('info', info)
+forwardConsole('warn', warn)
+forwardConsole('error', error)
+
+console.log('Registering Ag Grid...')
+
 ModuleRegistry.registerModules([AllCommunityModule])
 const queryClient = new QueryClient()
+
+console.log('Rendering React App...')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
