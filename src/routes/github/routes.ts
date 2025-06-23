dba7e1d0abcdbe66cc6f0ae67050fcffb3ea0d28
Maxime Bourand <maxime.bourand@oqee.tv>
2025-06-23 14:21:50 +0200

    Improve authentication by not requiring repo access anymore

--- src/routes/github/routes.ts
+++ src/routes/github/routes.ts
@@ -1,5 +1,5 @@
 import { ENV } from '../../Env'
 
 export const GITHUB_URLS = {
-  AUTHORIZE_APP: `${ENV.GITHUB_BASE_URL}/apps/drfr-translation-local/installations/new`
+  AUTHORIZE_APP: `${ENV.GITHUB_BASE_URL}/login/oauth/authorize?client_id=${ENV.GITHUB_APP_CLIENT_ID}`
 } as const
