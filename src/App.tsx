3d8e7ecca40c03ba971a965ca9f8ec5ceb9f7f16
Maxime Bourand <54736052+mbourand@users.noreply.github.com>
2025-07-22 21:21:56 +0200

    Nouvelle gestion des reviews (#20)
    * Save theme
    
    * Show reviewers in overview
    
    ---------
    
    Co-authored-by: Maxime Bourand <maxime.bourand@oqee.tv>

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { TRANSLATION_APP_PAGES } from './routes/pages/routes'
 import { HomeView } from './views/HomeView'
-import { lazy } from 'react'
+import { lazy, useEffect } from 'react'
 import { OverviewView } from './views/Overview/OverviewView'
 import { EditTranslationView } from './views/translation/edit/EditTranslationView'
 import { ReviewTranslationView } from './views/translation/review/ReviewTranslationView'
 import { stopOAuthServer } from './lib/oauth'
+import { store, STORE_KEYS } from './store/store'
 
 const AuthLoginView = lazy(() => import('./views/auth/AuthLoginView').then((m) => ({ default: m.AuthLoginView })))
 const AuthConfirmView = lazy(() => import('./views/auth/AuthConfirmView').then((m) => ({ default: m.AuthConfirmView })))
@@ -15,6 +16,14 @@ const AuthConfirmView = lazy(() => import('./views/auth/AuthConfirmView').then((
 stopOAuthServer()
 
 function App() {
+  useEffect(() => {
+    store.get<string>(STORE_KEYS.THEME).then((theme) => {
+      if (theme) {
+        document.querySelector('html')?.setAttribute('data-theme', theme)
+      }
+    })
+  }, [])
+
   return (
     <>
       <BrowserRouter>
--- src/views/Overview/TranslationList.tsx
+++ src/views/Overview/TranslationList.tsx
@@ -2,13 +2,15 @@ import { ReactNode } from 'react'
 import { To, useNavigate } from 'react-router'
 import { twMerge } from 'tailwind-merge'
 import { CheckIcon } from '../../components/icons/CheckIcon'
+import { CrossIcon } from '../../components/icons/CrossIcon'
 
 type TranslationListProps = {
   title: string
   translations: {
     id: number
     title: string
-    isApproved: boolean
+    approvals: string[]
+    requestedChanges: string[]
     author: string
     authorAvatar: string
     href: To
@@ -30,22 +32,15 @@ export const TranslationList = ({
   return (
     <div className={twMerge('rounded-box shadow-md border border-base-200', className)}>
       <h2 className="p-4 pb-2 text-xl font-semibold tracking-wide">{title}</h2>
-      <div className={twMerge('flex flex-col bg-base-100 rounded-box p-2 overflow-auto', flexClassName)}>
+      <div className={twMerge('flex flex-col bg-base-100 rounded-box p-2 overflow-auto gap-4', flexClassName)}>
         {translations.map((translation) => (
           <button
             key={translation.id}
-            className="border border-base-200 rounded-box p-4 group text-left mb-4 cursor-pointer hover:shadow-md"
+            className="border border-base-200 rounded-box p-4 group text-left cursor-pointer hover:shadow-md"
             onClick={() => navigate(translation.href)}
           >
             <h3 className="text-lg font-semibold group-hover:underline underline-offset-2 mb-4">{translation.title}</h3>
-            <div className="flex flex-row justify-between items-center gap-2">
-              {translation.isApproved ? (
-                <div className="text-success">
-                  <CheckIcon />
-                </div>
-              ) : (
-                <div />
-              )}
+            <div className="flex flex-row justify-end">
               <div className="flex items-center gap-2">
                 <p className="opacity-60 w-fit text-sm">par {translation.author}</p>
                 <div className="avatar w-8">
@@ -53,6 +48,46 @@ export const TranslationList = ({
                 </div>
               </div>
             </div>
+            {translation.approvals.length + translation.requestedChanges.length > 0 && (
+              <div className="flex flex-row mt-2">
+                {translation.approvals.length > 0 && (
+                  <>
+                    <div className="text-success mr-[2px]">
+                      <CheckIcon />
+                    </div>
+                    {translation.approvals.map((user) => (
+                      <div className="rounded-full min-w-[calc(24px-12px)] min-h-6 w-[calc(24px-12px)] h-6" key={user}>
+                        <div className="tooltip" data-tip={user}>
+                          <img
+                            className="rounded-full min-w-6 min-h-6 w-6 h-6"
+                            src={`https://github.com/${user}.png?size=128`}
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                )}
+                {translation.requestedChanges.length > 0 && (
+                  <div className={twMerge('flex flex-row', translation.approvals.length > 0 ? 'ml-4' : '')}>
+                    <div className="text-error mr-[2px]">
+                      <CrossIcon />
+                    </div>
+                    {translation.requestedChanges.map((user) => (
+                      <div className="rounded-full min-w-[calc(24px-12px)] min-h-6 w-[calc(24px-12px)] h-6" key={user}>
+                        <div className="tooltip" data-tip={user}>
+                          <img
+                            className="rounded-full min-w-6 min-h-6 w-6 h-6"
+                            src={`https://github.com/${user}.png?size=128`}
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            )}
           </button>
         ))}
         {extraElements}
